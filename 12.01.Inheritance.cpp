474aa8f5baf9f9b34765cecd98db145a9e877ca3
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-09-26 17:23:23 +0600

    Update 12.01.Inheritance.cpp

--- 12.01.Inheritance.cpp
+++ 12.01.Inheritance.cpp
@@ -94,3 +94,52 @@ int main() {
     // cout << d3.pub;        // ❌ ERROR (private in derived)
     return 0;
 }
+
+
+
+_______________________________________________________________________________________________________________________________________
+#include<iostream>
+using namespace std;
+class Base{
+    public:int pub=1;
+    protected:int prot=2;
+    private: int pri=3;
+
+};
+class DerivedPublic:public Base{
+    public:
+    void show(){
+        cout<<"pub="<<pub<<endl;
+        cout<<"prot="<<prot<<endl;
+        // cout<<"pri="<<pri<<endl;
+    }
+};
+class DerivedProtected:protected Base{
+    public:
+    void show(){
+        cout<<"pub="<<pub<<endl;
+        cout<<"prot="<<prot<<endl;
+        // cout<<"pri="<<pri<<endl;
+    }
+};
+class DerivedPrivate:private Base{
+    public:
+    void show(){
+         cout<<"pub="<<pub<<endl;
+        cout<<"prot="<<prot<<endl;
+        // cout<<"pri="<<pri<<endl;
+    }
+};
+int main(){
+    DerivedPublic dpublic;
+    dpublic.show();
+    DerivedProtected dprotected;
+    dprotected.show();
+    DerivedPrivate dprivate;
+    dprivate.show();
+    cout<<"----------------------------"<<endl;
+     cout<<dpublic.pub<<endl;
+    //  cout<<dpublic.prot<<endl;
+    // cout<<dpublic.pri<<endl;
+
+}
