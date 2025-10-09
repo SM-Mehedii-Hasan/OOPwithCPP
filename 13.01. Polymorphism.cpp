b76875e8346d7a3bb79e60c89c8cff597982babc
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-10-09 20:48:32 +0600

    Create 13.01. Polymorphism.cpp

---
+++ 13.01. Polymorphism.cpp
@@ -0,0 +1,19 @@
+🔹 What is Polymorphism?
+Polymorphism means "many forms".
+It allows one interface (function or operator) to act differently based on the object type that is using it.
+In C++, polymorphism allows you to perform a single action in different ways — for example,
+calling the same function name but performing different tasks depending on the object.
+🔸 Types of Polymorphism in C++
+C++ supports two main types of polymorphism:
+| Type                                      | When it happens          | Achieved By                                |
+| ----------------------------------------- | ------------------------ | ------------------------------------------ |
+| **1. Compile-time (Static) Polymorphism** | During compilation       | Function Overloading, Operator Overloading |
+| **2. Run-time (Dynamic) Polymorphism**    | During program execution | Virtual Functions (Function Overriding)    |
+
+______________________________________________________________________________________________________________________________________________________________________
+
+| Type                                      | Description                                                                | Example                                    |
+| ----------------------------------------- | -------------------------------------------------------------------------- | ------------------------------------------ |
+| **1. Compile-time (Static) Polymorphism** | The function call is resolved at **compile time**.                         | Function Overloading, Operator Overloading |
+| **2. Run-time (Dynamic) Polymorphism**    | The function call is resolved at **run time** using **virtual functions**. | Virtual Function (Method Overriding)       |
+
